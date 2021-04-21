29f5ef883e803638055dd966782aa45b99fc57ef
Ameba Brain <ameba.brain@gmail.com>
2021-04-21 03:39:16 +0300

    Adding custom database and table setup for mssql fdw

--- sql/postgres_mssql_setup.sql
+++ sql/postgres_mssql_setup.sql
@@ -1,9 +1,14 @@
 create schema tds_fdw;
 create extension tds_fdw schema tds_fdw;
-create server mssql foreign data wrapper tds_fdw options (servername 'mssql', port '1433');
+create server mssql foreign data wrapper tds_fdw options (servername 'mssql', port '1433', database 'test');
 create user mapping for postgres server mssql options (username 'sa', password 'mssql_2019');
 
 create foreign table banner(version text)
 server mssql
 options (query 'select @@version as version')
+;
+
+create foreign table users(id int, name varchar)
+server mssql
+options (query 'select * from users')
 ;
