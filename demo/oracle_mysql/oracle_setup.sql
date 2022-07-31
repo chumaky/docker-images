1a0d119e5e032cb37c0b03634a7320ad406fa232
Ameba Brain <ameba.brain@gmail.com>
2022-08-01 02:08:24 +0300

    Switching to current Oracle XE image. Working draft of HSQL between Oracle and MySQL

--- demo/oracle_mysql/oracle_setup.sql
+++ demo/oracle_mysql/oracle_setup.sql
@@ -1,8 +1,9 @@
+conn sys/admin@//localhost:1521/xepdb1 as sysdba
 create user test identified by test default tablespace users temporary tablespace temp quota unlimited on users;
 grant connect to test;
 grant create table to test;
 
 -- next commands have to be executed under "test" user
-conn test/test@//localhost:1521/orclpdb1.localdomain
+conn test/test@//localhost:1521/xepdb1
 create table t as select rownum id from dual connect by level <= 3;
 alter table t add constraint t_pk primary key (id);
